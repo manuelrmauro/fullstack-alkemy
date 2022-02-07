0fe783c83ab7ae6401784b02d3e1e09dcd06325b
Manuel <manuelrmauro@gmail.com>
2022-02-07 20:14:37 -0300

    feat/home

--- app/src/pages/Home/Home.jsx
+++ app/src/pages/Home/Home.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PieChart from '../../components/PieChart/PieChart.jsx';
 import Table from '../../components/Table/Table'
 
 function Home() {
+
+  const [input, setInput] = useState('balance')
+
+  const handleInputChange = (e) => {
+    e.preventDefault()
+    setInput(e.target.value)
+  }
+
 	return <div>
-    <PieChart title='Balance'></PieChart>
-    <PieChart title='Incomes by category'></PieChart>
-    <PieChart title='Expenses by category'></PieChart>
+    <div>
+      <label>Order by</label>
+      <select value={input} onChange={handleInputChange}>
+        <option value='balance'>Balance</option>
+        <option value='incomes'>Incomes</option>
+        <option value='expenses'>Expenses</option>
+      </select>
+    </div>
+    <div>
+    {input === 'balance' && <PieChart title='Balance'></PieChart>}
+    {input === 'incomes' && <PieChart title='Incomes'></PieChart>}
+    {input === 'expenses' && <PieChart title='Expenses'></PieChart>}
+    </div>
     <Table/>
   </div>;
 }
